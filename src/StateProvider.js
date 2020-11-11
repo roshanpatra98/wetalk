206864b6fb202b9fe792e1305accdd8f78411dc3
Sreyas Panda <62077431+sreyas649@users.noreply.github.com>
2020-11-11 20:48:00 +0530

    Update StateProvider.js

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,1 +1,12 @@
-Enter your JS code here
+//Sreyas
+import React, {createContext, useContext, useReducer} from "react";
+
+export const StateContext = createContext();
+
+export const StateProvider = ({reducer, initialState, children}) => (
+    <StateContext.Provider value = {useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+);
+
+export const useStateValue = () => useContext(StateContext);
